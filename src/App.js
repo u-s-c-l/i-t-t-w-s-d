2283306274c17c6790269a68be74b491d8211739
jalexxx <jumaanahalex@gmail.com>
2022-03-14 12:21:26 +0000

    imported and routed the pages on app.js - errors popped up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import { Header, Footer } from "./layout";
+import { About, Home, UserProfile } from "./pages";
+
 
 function App() {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/about"><About /></Route>
+        <Route path="/userprofile"><UserProfile /></Route>
+      </Switch>
       <Footer />
     </>
   );
