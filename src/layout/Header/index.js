77073e594e34752c81d167cacae37dfdc6d63691
jalexxx <jumaanahalex@gmail.com>
2022-03-14 12:37:06 +0000

    added colour and size to nav

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav>
+      <nav className="bg-indigo-500 w-full h-24">
         <NavLink exact to="/" activeClassName="active">Home</NavLink>
         <NavLink to="/about" activeClassName="active">About</NavLink>
         <NavLink to="/search" activeClassName="active">Search</NavLink>
