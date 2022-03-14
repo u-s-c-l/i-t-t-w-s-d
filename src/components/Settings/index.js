27c970d40d1a564ee9cbe0bb8ace8db3e47d07b8
jalexxx <jumaanahalex@gmail.com>
2022-03-14 17:42:20 +0000

    made the setting buttons - need to work on onclick

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Settings = () => {
   return (
-    <div>
-      <button id="settings_btn"> <i className="fa-solid fa-gear"></i> Here </button>
+    <div className="border border-indigo-600 rounded-full w-12 h-14 ">
+      <button id="settings_btn"> <i className="fa-solid fa-gear fa-2x absolute w-12 "></i> </button>
     </div>
   );
 };
