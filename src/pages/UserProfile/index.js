9530b2e789d3e25b1cf3568df2a1666472443037
jalexxx <jumaanahalex@gmail.com>
2022-03-14 11:16:14 +0000

    made the index js for home

---
+++ src/pages/UserProfile/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+const UserProfile = () => {
+  return (
+    <>
+      <div id="profile">
+        <div id="profile_buttons">
+          
+
+        </div>
+        
+  
+      </div>
+
+    </>
+  );
+};
+
+export default UserProfile;
