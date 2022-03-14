ec2b692b8c156bc78d64cd9281988250f470e9c2
jalexxx <jumaanahalex@gmail.com>
2022-03-14 11:30:34 +0000

    made index.js for footer

---
+++ src/layout/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => {
+  return (
+    <>
+      <div id="footer">
+      <h3> ©cutipihub </h3>
+      </div>
+
+    </>
+  );
+};
+
+export default Footer;
