4a6698fe2982848213dc59a00ddc06b6dedb9fac
ewhite1999 <ewhite1999@gmail.com>
2022-03-14 17:49:13 +0000

    fix reducer

--- src/reducers/quizReducer.js
+++ src/reducers/quizReducer.js
@@ -1,16 +1,15 @@
 const initState = {
-  category: "",
   questions: []
 };
 
 const quizReducer = (state = initState, action) => {
   switch (action.type) {
     case "LOADING":
-      return { ...state, location: action.payload, loading: true };
+      return { ...state, questions: action.payload, loading: true };
     case "GET_QUESTIONS":
       return {
         ...state,
-        location: action.payload,
+        questions: action.payload,
         loading: false,
         error: false
       };
