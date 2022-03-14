1be71373a9395f6c5dae7da3c641f744295d3ce3
jalexxx <jumaanahalex@gmail.com>
2022-03-14 12:11:23 +0000

    positioned footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div id="footer" className="bg-emerald-200 w-full h-24" >
+      <div id="footer" className="bg-emerald-200 absolute inset-x-0 bottom-0 w-full h-24" >
         <h3> ©cutipihub  </h3>
       </div>
 
