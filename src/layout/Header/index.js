02ac4474aca91ebf63709b48540b0a8b95a68f2c
jalexxx <jumaanahalex@gmail.com>
2022-03-14 11:26:32 +0000

    made div for header

---
+++ src/layout/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+  return (
+    <>
+      <div id="header">
+      <h3> ©cutipihub </h3>
+      </div>
+
+    </>
+  );
+};
+
+export default Header;
