48484ed6479fce23541043388e9640e63b24c5c3
saminakhan999 <samina.khan9999@gmail.com>
2022-03-15 22:20:32 +0000

    check page:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<></>} />
+        <Route path={"/"} element={<>hello world</>} />
         <Route path={"/quiz/*"} element={<QuizLanding />} />
         <Route path={"/quiz/create/*"} element={<Pages.Quiz />} />
       </Routes>
