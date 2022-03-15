7295ed434cf601884e2a819abaf1cdc56011e714
jalexxx <jumaanahalex@gmail.com>
2022-03-15 14:15:43 +0000

    made login file, indentation errors

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { default as Settings } from "./Settings";
 export { default as Chat } from "./Chat";
 export { default as Edit } from "./Edit";
 // // export { default as Register } from './Register';
-// export { default as Login } from './Login';
+export { default as Login } from './Login';
