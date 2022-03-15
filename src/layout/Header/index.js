8e9652f8b809b9e89d996bd2cdc033bf7832adca
jalexxx <jumaanahalex@gmail.com>
2022-03-15 22:40:08 +0000

    navlink for register/login, back button also

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -6,19 +6,19 @@ const Header = () => {
   return (
     <>
       <nav className="bg-indigo-500 w-full h-24  ">
-        <NavLink exact to="/" activeClassName="active"> Home</NavLink>
-        <NavLink activeClassName="current" to="/login">
-          Login
-        </NavLink>
-        <NavLink activeClassName="current" to="/register">
-          Register
-        </NavLink>
-        <NavLink to="/about" activeClassName="active">
-          About
-        </NavLink>
-        <NavLink to="/userprofile" activeClassName="active">
-          Profile
-        </NavLink>
+        <NavLink exact to="/" activeClassName="active"> Home </NavLink>
+        <NavLink to="/about" activeClassName="active"> About </NavLink>
+        { !currentUser ?
+                <>
+                <NavLink activeClassName='current' to='/login'>Login</NavLink>
+                <NavLink activeClassName='current' to='/register'>Register</NavLink>
+                </>
+                : 
+                <>
+                <NavLink to="/userprofile" activeClassName="active"> Profile </NavLink>
+                <button onClick={logout}>Logout</button>
+                </>
+            }
       </nav>
     </>
   );
