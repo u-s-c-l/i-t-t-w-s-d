6a188cd6efd8bb2fb49108d97d721126018d45bb
ewhite1999 <ewhite1999@gmail.com>
2022-03-15 11:00:46 +0000

    fix keys

--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import shuffle from "./helpers";
+import { v4 as uuidv4 } from "uuid";
 
 const Answers = ({ correct_answer, incorrect_answers, handleChange }) => {
   const allAnswers = [...incorrect_answers, correct_answer];
@@ -10,13 +11,14 @@ const Answers = ({ correct_answer, incorrect_answers, handleChange }) => {
       {shuffledAns.map((ans, index) => {
         const consistentName = ans.replaceAll(" ", "").toLowerCase();
         return (
-          <div key={consistentName}>
+          <div key={uuidv4()}>
             <label htmlFor={consistentName}>{ans}</label>
             <input
               type={"radio"}
               id={consistentName}
               name={`answer_${index}`}
               value={consistentName}
+              data-correct={ans === correct_answer}
             />
           </div>
         );
