21eb1c2cdccf0028c54c65236172b6f82b1be03a
jalexxx <jumaanahalex@gmail.com>
2022-03-15 09:21:44 +0000

    made an edit folder

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -3,7 +3,9 @@ import React from "react";
 const Settings = () => {
   return (
     <div>
-      <button id="settings_btn"> <i className="fa-solid fa-gear fa-2x"></i> </button>
+      <button id="settings_btn">
+        {/* <i className="fa-solid fa-gear fa-2x"> </i>  */}
+      </button>
     </div>
   );
 };
