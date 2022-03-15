cb842690ff7a4619beaddfda43a1e791e4611c36
jalexxx <jumaanahalex@gmail.com>
2022-03-15 09:40:34 +0000

    made chat div

---
+++ src/components/Chat/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div>
+      <button id="chat_btn">
+        {/* <i className="fa-solid fa-user-pen"></i> */}
+      </button>
+    </div>
+  );
+};
+
+export default Chat; 
