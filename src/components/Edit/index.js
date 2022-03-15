f2d493bb2cf996b6097f90a75bffda6488fecc01
jalexxx <jumaanahalex@gmail.com>
2022-03-15 09:23:08 +0000

    made div for the edit btn

---
+++ src/components/Edit/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Edit = () => {
+  return (
+    <div>
+      <button id="edit_btn">
+        {/* <i className="fa-solid fa-gear fa-2x"> </i>  */}
+      </button>
+    </div>
+  );
+};
+
+export default Edit; 
