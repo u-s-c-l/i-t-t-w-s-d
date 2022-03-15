fee425fe8e17eeeee018007b457dbe018992b702
saminakhan999 <samina.khan9999@gmail.com>
2022-03-15 10:43:43 +0000

    renamed to socketContext

--- src/components/SocketContext/SocketContext.js
+++ src/components/SocketContext/SocketContext.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react"
 import socketio from "socket.io-client"
 
+// this is for the SocketContext Provider
+
 export const SocketContext = React.createContext();
