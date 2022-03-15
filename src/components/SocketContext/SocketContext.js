27b13854b0ffe9a14a492040b70d08ed6da447be
saminakhan999 <samina.khan9999@gmail.com>
2022-03-15 11:05:39 +0000

    created socket provider

--- src/components/SocketContext/SocketContext.js
+++ src/components/SocketContext/SocketContext.js
@@ -1,6 +1,35 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import socketio from "socket.io-client"
 
-// this is for the SocketContext Provider
 
-export const SocketContext = React.createContext();
+// this is to use the useSocket hook on required buttons
+const SocketContext = React.createContext();
+
+export const useSocket = useContext(SocketContext)
+
+// this is for the SocketContext Provider 
+
+const SocketProvider = (props) => {
+
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    const ENDPOINT = "https://localhost:3003"
+    const newSocket = 
+    setSocket(io(ENDPOINT))
+    return () => newSocket.close();
+  }, [setSocket])
+
+  if (!socket) {
+    return <div>Loading...</div>
+  }
+
+  return (
+
+    <SocketContext.Provider value={socket}>
+      {props.children}
+    </SocketContext.Provider>
+  )
+}
+
+export default SocketProvider
