e72f9daf76d6af6906bc7756ccbd0b0fef7eb3af
ewhite1999 <ewhite1999@gmail.com>
2022-03-15 11:41:19 +0000

    create landing page

---
+++ src/components/QuizLanding/QuizLanding.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const QuizLanding = () => {
+  return (
+    <>
+      <Link to="/create">Create Game</Link>
+    </>
+  );
+};
+
+export default QuizLanding;
