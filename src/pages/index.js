7b5446874a816ce09a62e909c12704d51d5959b9
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 12:06:34 +0000

    render leaderboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Quiz } from "./Quiz";
 export { default as About } from "./About";
 export { default as Home } from "./Home";
 export { default as UserProfile } from "./UserProfile";
+export { default as LeaderBoard } from "./LeaderBoard";
