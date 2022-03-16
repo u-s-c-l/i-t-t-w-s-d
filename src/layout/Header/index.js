f473d5632e7ae55dc25186f2949b7cadefd20726
jalexxx <jumaanahalex@gmail.com>
2022-03-16 15:24:55 +0000

    fixed navlinks

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -3,38 +3,27 @@ import { NavLink } from "react-router-dom";
 import { useAuthContext } from "../../contexts";
 
 const Header = () => {
-  return (
-    <>
-      <nav className="bg-indigo-500 w-full h-24  ">
-        <NavLink exact to="/" activeClassName="active">
-          {" "}
-          Home{" "}
-        </NavLink>
-        <NavLink to="/about" activeClassName="active">
-          {" "}
-          About{" "}
-        </NavLink>
-        {!currentUser ? (
-          <>
-            <NavLink activeClassName="current" to="/login">
-              Login
-            </NavLink>
-            <NavLink activeClassName="current" to="/register">
-              Register
-            </NavLink>
-          </>
-        ) : (
-          <>
-            <NavLink to="/userprofile" activeClassName="active">
-              {" "}
-              Profile{" "}
-            </NavLink>
-            <button onClick={logout}>Logout</button>
-          </>
-        )}
-      </nav>
-    </>
-  );
+  const { currentUser, logout } = useAuthContext();
+  
+    return (
+      <>
+        <nav className="bg-indigo-500 w-full h-24  ">
+          <NavLink exact to="/" activeClassName="active">Home</NavLink>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+          {!currentUser ? 
+            <>
+              <NavLink activeClassName="current" to="/login">Login </NavLink>
+              <NavLink activeClassName="current" to="/register">Register</NavLink>
+            </>
+            : 
+            <>
+              <NavLink to="/userprofile" activeClassName="active">Profile</NavLink>
+              <button onClick={logout}>Logout</button>
+            </>
+          }
+        </nav>
+      </>
+    );
 };
 
 export default Header;
