b7c63e277e0a24492145449722da7b443860f8fe
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 10:28:55 +0000

    refactor header

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -6,19 +6,32 @@ const Header = () => {
   return (
     <>
       <nav className="bg-indigo-500 w-full h-24  ">
-        <NavLink exact to="/" activeClassName="active"> Home </NavLink>
-        <NavLink to="/about" activeClassName="active"> About </NavLink>
-        { !currentUser ?
-                <>
-                <NavLink activeClassName='current' to='/login'>Login</NavLink>
-                <NavLink activeClassName='current' to='/register'>Register</NavLink>
-                </>
-                : 
-                <>
-                <NavLink to="/userprofile" activeClassName="active"> Profile </NavLink>
-                <button onClick={logout}>Logout</button>
-                </>
-            }
+        <NavLink exact to="/" activeClassName="active">
+          {" "}
+          Home{" "}
+        </NavLink>
+        <NavLink to="/about" activeClassName="active">
+          {" "}
+          About{" "}
+        </NavLink>
+        {!currentUser ? (
+          <>
+            <NavLink activeClassName="current" to="/login">
+              Login
+            </NavLink>
+            <NavLink activeClassName="current" to="/register">
+              Register
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to="/userprofile" activeClassName="active">
+              {" "}
+              Profile{" "}
+            </NavLink>
+            <button onClick={logout}>Logout</button>
+          </>
+        )}
       </nav>
     </>
   );
