428b5ee4acfb330bf4fd1cdc159c444f97773c43
jalexxx <jumaanahalex@gmail.com>
2022-03-16 15:04:41 +0000

    made a routing foldera

---
+++ routing/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import { useAuthContext } from "../contexts/auth";
+
+export const LoggedOutRoute = ({ children }) => {
+    const { currentUser } = useAuthContext();
+
+    return (
+        <Route> { 
+            !currentUser
+                ? children
+                : <Redirect to='/userprofile' /> }
+        </Route> )
+}
+
+
+export const PrivateRoute = ({ children }) => {
+    const { currentUser } = useAuthContext();
+
+    return (
+        <Route> { 
+            !!currentUser
+                ? children
+                : <Redirect to='/login' /> }
+        </Route> )
+}
+
