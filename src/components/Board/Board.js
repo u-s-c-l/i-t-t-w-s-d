6a5c6192d32fae04d3d3bd882f0bd6f23a6b897b
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 12:06:02 +0000

    just return table body

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,22 +4,13 @@ import PropTypes from "prop-types";
 const Board = ({ data }) => {
   const { cat, username, score } = data;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Category:</th>
-          <th>Username:</th>
-          <th>Score:</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{cat}</td>
-          <td>{username}</td>
-          <td>{score}</td>
-        </tr>
-      </tbody>
-    </table>
+    <tbody>
+      <tr>
+        <td>{cat}</td>
+        <td>{username}</td>
+        <td>{score}</td>
+      </tr>
+    </tbody>
   );
 };
 
