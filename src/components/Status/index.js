04de1c98b06db78e898589496192d69d81b96d6b
jalexxx <jumaanahalex@gmail.com>
2022-03-16 23:53:00 +0000

    completed status component

--- src/components/Status/index.js
+++ src/components/Status/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Status = 
-);
+const Status = ({ onChange, value }) => {
+  return (
+    <div className="field">
+      <label htmlFor="status">status:</label>
+      <input
+        id="status"
+        type="text"
+        onChange={onChange}
+        maxLength="35"
+        value={value}
+        placeholder="It's a nice day!"
+        required
+      />
+    </div>
+  );
+};
 
 Status.propTypes = {
   onSubmit: PropTypes.func,
