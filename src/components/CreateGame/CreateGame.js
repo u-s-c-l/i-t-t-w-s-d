432f7e8ea5154e3a73957728a6094f7096765a24
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 15:38:07 +0000

    use state to only show next question after first one answered

--- src/components/CreateGame/CreateGame.js
+++ src/components/CreateGame/CreateGame.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CreateGame = ({ getQuestions, startGame }) => {
+  const [selected, setSelected] = useState(0);
+
   const categories = [
     "animals",
     "general knowledge",
@@ -12,6 +14,12 @@ const CreateGame = ({ getQuestions, startGame }) => {
 
   const difficulties = ["easy", "medium", "hard"];
 
+  const setFirstInput = () => {
+    setSelected(1);
+  };
+  const setSecondInput = () => {
+    setSelected(2);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -27,50 +35,80 @@ const CreateGame = ({ getQuestions, startGame }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} aria-label="create game form">
-      <h3 className="capitalize">Select a category:</h3>
-      {categories.map((cat, index) => {
-        const consistentName = cat.replaceAll(" ", "").toLowerCase();
-        return (
-          <div key={`cat_${index}`}>
-            <label htmlFor={consistentName} className="capitalize">
-              {cat}
-            </label>
-            <input
-              type={"radio"}
-              id={consistentName}
-              name="category"
-              value={consistentName}
-              required
-            />
-          </div>
-        );
-      })}
+    <div className=" text-tblack">
+      <h2 className="text-2xl font-bold ">Create A Game</h2>
+      <form
+        onSubmit={handleSubmit}
+        aria-label="create game form"
+        className=" container flex flex-col space-y-4"
+      >
+        <>
+          <h3 className="capitalize text-center text-2xl font-medium">
+            Select a category:
+          </h3>
 
-      <h3 className="capitalize">Select a difficulty:</h3>
-      {difficulties.map((diff, index) => {
-        return (
-          <div key={`diff_${index}`}>
-            <label htmlFor={diff} className="capitalize">
-              {diff}
-            </label>
-            <input
-              required
-              type={"radio"}
-              id={diff}
-              name="difficulty"
-              value={diff}
-            />
-          </div>
-        );
-      })}
+          {categories.map((cat, index) => {
+            const consistentName = cat.replaceAll(" ", "").toLowerCase();
+            return (
+              <div key={`cat_${index}`}>
+                <input
+                  type={"radio"}
+                  id={consistentName}
+                  name="category"
+                  value={consistentName}
+                  required
+                  className="peer hidden"
+                  onChange={setFirstInput}
+                />
+                <label
+                  htmlFor={consistentName}
+                  className="cursor-pointer capitalize bg-gradient-to-r from-tpink opacity-70 to-torange text-white py-3 text-center rounded-xl font-bold w-60 mx-auto block peer-checked:opacity-100 hover:opacity-100"
+                >
+                  {cat}
+                </label>
+              </div>
+            );
+          })}
+        </>
 
-      <input
-        aria-label="submit btn"
-        type={"submit"}
-        value="Create Game!"
-      ></input>
-    </form>
+        {selected > 0 && (
+          <>
+            <h3 className="capitalize text-center text-2xl font-medium">
+              Select a difficulty:
+            </h3>
+            {difficulties.map((diff, index) => {
+              return (
+                <div key={`diff_${index}`}>
+                  <input
+                    required
+                    type={"radio"}
+                    id={diff}
+                    name="difficulty"
+                    value={diff}
+                    className="hidden peer"
+                    onChange={setSecondInput}
+                  />
+                  <label
+                    htmlFor={diff}
+                    className="cursor-pointer capitalize bg-gradient-to-r from-tpink opacity-70 to-torange text-white py-3 text-center rounded-xl font-bold w-60 mx-auto block peer-checked:opacity-100 hover:opacity-100"
+                  >
+                    {diff}
+                  </label>
+                </div>
+              );
+            })}
+          </>
+        )}
+        {selected === 2 && (
+          <input
+            aria-label="submit btn"
+            type={"submit"}
+            value="Start Game!"
+            className="capitalize bg-gradient-to-r from-tpink to-torange text-white py-3 text-center rounded-full font-bold w-60 mx-auto block opacity-70 cursor-pointer hover:opacity-100 disabled:hidden"
+          ></input>
+        )}
+      </form>
+    </div>
   );
 };
 
