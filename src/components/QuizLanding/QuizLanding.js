66fab22f11d2a49b3045192d31f56ce71ff143a0
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 15:04:51 +0000

    change QuizLanding btn

--- src/components/QuizLanding/QuizLanding.js
+++ src/components/QuizLanding/QuizLanding.js
@@ -8,14 +8,14 @@ const QuizLanding = () => {
         <Link
           role="link"
           to="/quiz/create"
-          className=" bg-gradient-to-r from-tpink to-torange text-white py-2 text-center rounded-full font-bold w-52 mx-auto"
+          className="bg-gradient-to-r from-tpink to-torange text-white py-3 text-center rounded-full font-bold w-60 mx-auto"
         >
           Create a Game
         </Link>
         <Link
           role="link"
           to="/quiz/join"
-          className=" bg-gradient-to-r from-tpink to-torange text-white py-2 text-center rounded-full font-bold w-52 mx-auto"
+          className="bg-gradient-to-r from-tpink to-torange text-white py-3 text-center rounded-full font-bold w-60 mx-auto"
         >
           Join a Game
         </Link>
