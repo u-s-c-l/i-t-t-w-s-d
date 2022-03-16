aae1cbf5ca9f0ab925b3b45b6324fd6d0865d319
jalexxx <jumaanahalex@gmail.com>
2022-03-16 23:51:09 +0000

    added return to edit index.js

--- src/components/Edit/index.js
+++ src/components/Edit/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Edit = ({ onSubmit, children }) => (
-  <div className="card">
-    <form onSubmit={onSubmit}>
-      <h1>Profile Card</h1>
-      {children}
-      <button type="submit" className="save">
-        Save{" "}
-      </button>
-    </form>
-  </div>
-);
+const Edit = ({ onSubmit, children }) => {
+  return (
+    <div className="card">
+      <form onSubmit={onSubmit}>
+        <h1>Profile Card</h1>
+        {children}
+        <button type="submit" className="save">
+          Save{" "}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 Edit.propTypes = {
   onSubmit: PropTypes.func,
---
+++ src/components/Status/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Status = 
+);
+
+Status.propTypes = {
+  onSubmit: PropTypes.func,
+  children: PropTypes.object
+};
+export default Status;
