af0d0c08906985807423bde634483f59db95c5b5
jalexxx <jumaanahalex@gmail.com>
2022-03-16 23:55:13 +0000

    exported status

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as SettingsBtn } from "./SettingsBtn";
 export { default as Chat } from "./Chat";
 export { default as Edit } from "./Edit";
+export { default as Status } from "./Status";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
