7bcf9c82005388d0d9af71bd36453599ff6f71a0
Noah Platton <noahplatton@gmail.com>
2022-03-16 18:26:45 +0000

    Remove unneccessary routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Header, Footer } from "./layout";
-import * as CustomRoutes from "./routing";
 import QuizLanding from "./components/QuizLanding";
 import { Routes, Route } from "react-router-dom";
 import * as Pages from "./pages";
+import { useAuthContext } from "./contexts";
 
 function App() {
+  const { currentUser } = useAuthContext();
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path={"/"} element={<Pages.Home />} />
         <Route path={"/about"} element={<Pages.About />} />
-        <CustomRoutes.LoggedOutRoute path="/login"><Pages.About /></CustomRoutes.LoggedOutRoute>
-        <CustomRoutes.LoggedOutRoute path="/register"><Pages.About /></CustomRoutes.LoggedOutRoute>
+
+        {/* {!currentUser ? (
+          <>
+            <Route path={"/login"} element={<Pages.About />} />
+            <Route path={"/register"} element={<Pages.About />} />
+          </>
+        ) : (
+          <></>
+        )} */}
+
         <Route path={"/quiz/*"} element={<QuizLanding />} />
         <Route path={"/quiz/create/*"} element={<Pages.Quiz />} />
       </Routes>
