a979b5d0ca842e734766459d78766683eb03b25f
Noah Platton <noahplatton@gmail.com>
2022-03-16 18:27:54 +0000

    Comment out unneeded code

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useAuthContext } from "../contexts/auth";
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { Route, Navigate } from "react-router-dom";
+// import { useAuthContext } from "../contexts/index";
 
-export const LoggedOutRoute = ({ children }) => {
-    const { currentUser } = useAuthContext();
+// export const LoggedOutRoute = ({ children }) => {
+//   const { currentUser } = useAuthContext();
 
-    return (
-        <Route> { 
-            !currentUser
-                ? children
-                : <Redirect to='/userprofile' /> }
-        </Route> ) 
-}
+//   return (
+//     <Route> {!currentUser ? children : <Navigate to="/userprofile" />}</Route>
+//   );
+// };
 
+// export const PrivateRoute = ({ children }) => {
+//   const { currentUser } = useAuthContext();
 
-export const PrivateRoute = ({ children }) => {
-    const { currentUser } = useAuthContext();
+//   return <Route> {currentUser ? children : <Navigate to="/login" />}</Route>;
+// };
 
-    return (
-        <Route> { 
-            !!currentUser
-                ? children
-                : <Redirect to='/login' /> }
-        </Route> )
-}
+// LoggedOutRoute.propTypes = {
+//   children: PropTypes.object.isRequired
+// };
 
+// PrivateRoute.propTypes = {
+//   children: PropTypes.object.isRequired
+// };
