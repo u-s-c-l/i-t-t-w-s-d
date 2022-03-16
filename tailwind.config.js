34ae9331311d65336fd7f3787cb3910e1751b1fc
ewhite1999 <ewhite1999@gmail.com>
2022-03-16 12:48:13 +0000

    add tinder colours to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        tpink: "#FD267A",
+        torange: "#FF7854",
+        tblack: "#424242"
+      }
+    }
   },
   plugins: []
 };
