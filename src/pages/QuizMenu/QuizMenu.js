daded84d0c52c0a206dd7446524dac2343605011
ewhite1999 <ewhite1999@gmail.com>
2022-03-17 16:48:23 +0000

    tell a player to join a game

--- src/pages/QuizMenu/QuizMenu.js
+++ src/pages/QuizMenu/QuizMenu.js
@@ -1,13 +1,16 @@
 import React from "react";
-import  QuizLanding  from "../../components/QuizLanding";
-
+import QuizLanding from "../../components/QuizLanding";
 
 const QuizMenu = () => {
-
   return (
     <>
-      <h1> Quiz Menu </h1>
-      <QuizLanding/>
+      <h1 className="capitalize text-center text-4xl font-bold text-slate-50 pt-10">
+        Quiz Menu{" "}
+      </h1>
+      <p className="text-center m-6 text-xl text-slate-50 max-w-prose mb-4">
+        Either join a game or start your own
+      </p>
+      <QuizLanding />
     </>
   );
 };
