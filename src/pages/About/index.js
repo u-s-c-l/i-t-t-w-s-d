fde39847a220343b0bf196d3c556107b425633ca
ewhite1999 <ewhite1999@gmail.com>
2022-03-17 15:22:43 +0000

    fake about text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -22,7 +22,20 @@ function About() {
       <h1 className="capitalize text-center text-4xl font-bold text-slate-50">
         I thought this was speed dating
       </h1>
-      <p></p>
+      <div className="m-8">
+        <p className="text-slate-50 max-w-prose mb-4">
+          A wonderful serenity has taken possession of my entire soul, like
+          these sweet mornings of spring which I enjoy with my whole heart. I am
+          alone, and feel the charm of existence in this spot, which was created
+          for the bliss of souls like mine.
+        </p>
+        <p className="text-slate-50 max-w-prose">
+          I am so happy, my dear friend, so absorbed in the exquisite sense of
+          mere tranquil existence, that I neglect my talents. I should be
+          incapable of drawing a single stroke at the present moment; and yet I
+          feel that I never was a greater artist than now.
+        </p>
+      </div>
       <div className="m-8 rounded-lg overflow-hidden bg-slate-50 border-2 border-tblack">
         <div className="flex justify-between">
           <button
