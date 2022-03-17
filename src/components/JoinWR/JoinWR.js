1f08ca59e113615c367258ee953f28e63b613f8c
ewhite1999 <ewhite1999@gmail.com>
2022-03-17 17:46:45 +0000

    ignore eslint inden on join WR

--- src/components/JoinWR/JoinWR.js
+++ src/components/JoinWR/JoinWR.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React, { useEffect } from "react";
 import { useSocket } from "../../components/SocketContext/SocketContext";
 import { useDispatch } from "react-redux";
