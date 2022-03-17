182eb978ddb43411b41dd46595e560c233db70c9
jalexxx <jumaanahalex@gmail.com>
2022-03-17 00:07:33 +0000

    exported profile

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as Chat } from "./Chat";
 export { default as Edit } from "./Edit";
 export { default as Status } from "./Status";
 export { default as Name } from "./Name";
+export { default as Profile } from "./Profile";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
