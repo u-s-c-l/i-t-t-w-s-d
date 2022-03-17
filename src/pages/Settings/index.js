54ad087f020117be6ea6e864aa2ca33b73edaaa0
jalexxx <jumaanahalex@gmail.com>
2022-03-17 14:57:21 +0000

    made the profile in settings toggle list

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Settings = () => {
+  const [show, setShow] = useState(true);
+
   return (
     <div className="settings-div">
       {/* <a href="#" className="nav-link">
@@ -10,16 +12,21 @@ const Settings = () => {
         </div>
       </a> */}
       <a id="profile_btn" href="#">
-        <i className="far fa-user-circle"></i> Profile
-        <div className="dropdown">
-          <ul>
-            <li>Option 1</li>
-            <li>Option 2</li>
-            <li>Option 3</li>
-            <li>Option 4</li>
-          </ul>
-        </div>
+        <i className="far fa-user-circle" onClick={() => setShow((s) => !s)}>
+          Profile{" "}
+        </i>{" "}
       </a>
+      <div
+        className="dropdown"
+        style={{ visibility: show ? "visible" : "hidden" }}
+      >
+        <ul>
+          <li>Option 1</li>
+          <li>Option 2</li>
+          <li>Option 3</li>
+          <li>Option 4</li>
+        </ul>
+      </div>
     </div>
   );
 };
