49650bf9ce7936a2154e67c18f5b7634661331eb
jalexxx <jumaanahalex@gmail.com>
2022-03-17 01:55:25 +0000

    made the nav links for the side menu

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -3,7 +3,18 @@ import React from "react";
 const Settings = () => {
   return (
     <div className="side-menu">
-      <h1> hello</h1>
+      <a href="#" className="nav-link">
+        <i className="far fa-user-circle"></i> Profile
+      </a>
+      <a href="#" className="nav-link">
+        <i className="far fa-bell"></i> Notifications
+      </a>
+      <a href="#" className="nav-link">
+        <i className="far fa-file-alt"></i> Billing Info
+      </a>
+      <a href="#" className="nav-link">
+        <i className="fas fa-cogs"></i> General
+      </a>
     </div>
   );
 };
