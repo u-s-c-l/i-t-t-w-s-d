c6e7420da665dc7c5682f286b829d01970a736ef
jalexxx <jumaanahalex@gmail.com>
2022-03-17 02:32:22 +0000

    made checkbox fot the lightmode / dark mode

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -5,18 +5,31 @@ const Settings = () => {
     <div className="settings-div">
       <a href="#" className="nav-link">
         <i className="far fa-user-circle"></i> Profile
+        <div className="dropdown"> </div>
       </a>
       <a href="#" className="nav-link">
         <i className="far fa-bell"></i> Account
+        <div className="dropdown">
+          <div className="setting_name"> Lightmode/dark Mode</div>
+          <input
+            type="checkbox"
+            id="setting_name"
+            name="setting_name"
+            value=" "
+          ></input>
+        </div>
       </a>
       <a href="#" className="nav-link">
         <i className="fa fa-lock"></i> Privacy
+        <div className="dropdown"> </div>
       </a>
       <a href="#" className="nav-link">
         <i className="fas fa-cogs"></i> General
+        <div className="dropdown"> </div>
       </a>
       <a href="#" className="nav-link">
         <i className="fa fa-question"></i> Delete Account
+        <div className="dropdown"> </div>
       </a>
     </div>
   );
