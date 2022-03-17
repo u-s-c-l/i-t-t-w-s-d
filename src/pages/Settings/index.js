15fbafcc9220a0e6356568a93bb4235bdabadbbd
jalexxx <jumaanahalex@gmail.com>
2022-03-17 02:11:11 +0000

    changed icons for pages on settings

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -10,13 +10,13 @@ const Settings = () => {
         <i className="far fa-bell"></i> Account
       </a>
       <a href="#" className="nav-link">
-        <i className="far fa-file-alt"></i> Privacy
+        <i className="fa fa-lock"></i> Privacy
       </a>
       <a href="#" className="nav-link">
         <i className="fas fa-cogs"></i> General
       </a>
       <a href="#" className="nav-link">
-        <i className="fas fa-cogs"></i> Delete Account
+        <i className="fa fa-question"></i> Delete Account
       </a>
     </div>
   );
