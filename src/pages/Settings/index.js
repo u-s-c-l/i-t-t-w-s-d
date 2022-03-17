37b31653be36a90d841caf510239491821b598ea
jalexxx <jumaanahalex@gmail.com>
2022-03-17 14:22:07 +0000

    make a dropdown div , with unlisted list

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -3,102 +3,22 @@ import React from "react";
 const Settings = () => {
   return (
     <div className="settings-div">
-      <a href="#" className="nav-link">
+      {/* <a href="#" className="nav-link">
         <i className="far fa-user-circle"></i> Profile
         <div className="dropdown">
           <div className="setting_name"> Lightmode/dark Mode </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Changing Avatar</div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
         </div>
-      </a>
-      <a href="#" className="nav-link">
-        <i className="far fa-bell"></i> Account
+      </a> */}
+      <a id="profile_btn" href="#">
+        <i className="far fa-user-circle"></i> Profile
         <div className="dropdown">
-          <div className="setting_name"> Chat Notifications </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Game Notifications </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Change Password </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Change Username </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="Dropdown_billInfo">
-            <div className="setting_name"> bill information </div>
-          </div>
+          <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+            <li>Option 4</li>
+          </ul>
         </div>
-      </a>
-      <a href="#" className="nav-link">
-        <i className="fa fa-lock"></i> Privacy
-        <div className="dropdown">
-          <div className="setting_name"> Aceept Chat requests </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Anonymous mode </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-        </div>
-      </a>
-      <a href="#" className="nav-link">
-        <i className="fas fa-cogs"></i> General
-        <div className="dropdown">
-          <div className="setting_name"> Help </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-          <div className="setting_name"> Compeition alerts </div>
-          <input
-            type="checkbox"
-            id="setting_name"
-            name="setting_name"
-            value=" "
-          ></input>
-        </div>
-      </a>
-      <a href="#" className="nav-link">
-        <i className="fa fa-question"></i> Delete Account
-        <button> delete </button>
       </a>
     </div>
   );
