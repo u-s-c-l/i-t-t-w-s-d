00141b4e8fe109194506e3b03200fbf6064e5b7d
saminakhan999 <samina.khan9999@gmail.com>
2022-03-17 04:14:02 +0000

    made startgame function

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,18 +10,30 @@ const loadQuestions = (questions, difficulty) => ({
 
 // combined getQuestions with startGame function
 
-const getQuestions = async ( category, difficulty ) => {
-  // const categoryMap = {
-  //   generalknowledge: 9,
-  //   animals: 27,
-  //   film: 11,
-  //   gadgets: 30,
-  //   music: 12
-  // };
-  // const catId = categoryMap[category];
+export const startGame = (category, difficulty) => {
+  const categoryMap = {
+    generalknowledge: 9,
+    animals: 27,
+    film: 11,
+    gadgets: 30,
+    music: 12
+  };
+  const catId = categoryMap[category];
 
-  // return async (dispatch) => {
-  //   dispatch(loading(category));
+  return async (dispatch) => {
+    dispatch(loading(category));
+    try {
+      const questions = await fetchQuestions(catId, difficulty);
+      console.log(questions);
+      dispatch(loadQuestions(questions));
+    } catch (err) {
+      console.warn(err.message);
+      dispatch({ type: "SET_ERROR", payload: err.message });
+    }
+  };
+};
+
+const getQuestions = async ( category, difficulty ) => {
 
   try {
     const { data } = await axios.get(
