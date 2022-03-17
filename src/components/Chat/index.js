2cdcbccf0dcfd42a65794bc266a4ddbf79017ec1
jalexxx <jumaanahalex@gmail.com>
2022-03-17 01:58:56 +0000

    fixed route for chatroom in chat component

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Chat = () => {
   return (
     <div>
-      <a id="chat_btn" href="/pages/ChatRoom">
+      <a id="chat_btn" href="/ChatRoom">
         <i className="fa-solid fa-hand-holding-heart"></i>
       </a>
     </div>
