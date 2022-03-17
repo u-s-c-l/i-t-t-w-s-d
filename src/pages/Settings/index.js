52cea08c4894531dd57f9c7c7a35fc89908db225
jalexxx <jumaanahalex@gmail.com>
2022-03-17 02:38:42 +0000

    made another dropdwon for bill info and finished checkboxes for account

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -25,20 +25,50 @@ const Settings = () => {
       <a href="#" className="nav-link">
         <i className="far fa-bell"></i> Account
         <div className="dropdown">
-          <div className="setting_name"> Lightmode/dark Mode </div>
+          <div className="setting_name"> Chat Notifications </div>
           <input
             type="checkbox"
             id="setting_name"
             name="setting_name"
             value=" "
           ></input>
-          <div className="setting_name"> Changing Avatar</div>
+          <div className="setting_name"> Game Notifications </div>
           <input
             type="checkbox"
             id="setting_name"
             name="setting_name"
             value=" "
           ></input>
+          <div className="setting_name"> Accept Chat Requests </div>
+          <input
+            type="checkbox"
+            id="setting_name"
+            name="setting_name"
+            value=" "
+          ></input>
+          <div className="setting_name"> Change Password </div>
+          <input
+            type="checkbox"
+            id="setting_name"
+            name="setting_name"
+            value=" "
+          ></input>
+          <div className="setting_name"> Change Username </div>
+          <input
+            type="checkbox"
+            id="setting_name"
+            name="setting_name"
+            value=" "
+          ></input>
+          <div className="Dropdown_billInfo">
+            <div className="setting_name"> bill information </div>
+            <input
+              type="checkbox"
+              id="setting_name"
+              name="setting_name"
+              value=" "
+            ></input>
+          </div>
         </div>
       </a>
       <a href="#" className="nav-link">
