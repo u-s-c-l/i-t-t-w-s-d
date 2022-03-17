6192713bcba0e98573ed56ecf0d337545ad738ba
Noah Platton <noahplatton@gmail.com>
2022-03-17 13:38:39 +0000

    Add tests for Board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const Board = ({ data }) => {
   const { cat, username, score } = data;
   return (
-    <tbody>
+    <tbody role="table">
       <tr>
         <td>{cat}</td>
         <td>{username}</td>
