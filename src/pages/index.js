60a0421b6066ce2fb6fa99d47e6130fd36284f35
jalexxx <jumaanahalex@gmail.com>
2022-03-17 01:33:06 +0000

    exported chatroom

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Home } from "./Home";
 export { default as UserProfile } from "./UserProfile";
 export { default as LeaderBoard } from "./LeaderBoard";
 export { default as Settings } from "./Settings";
+export { default as ChatRoom } from "./ChatRoom";
