a5205b30866e51430079c99ac5a973e577575c32
jalexxx <jumaanahalex@gmail.com>
2022-03-18 10:34:22 +0000

    added margin

--- src/components/SettingsBtn/index.js
+++ src/components/SettingsBtn/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SettingsBtn = () => {
   return (
-    <div className="bg-slate-50 w-28 flex justify-around rounded-3xl max-w-sm">
+    <div className="bg-slate-50 mt-8 ml-8 w-28 flex justify-around rounded-3xl max-w-sm">
       <a id="settings_btn" href="/Settings">
         <i className="fa-solid fa-gear fa-2x "></i>
       </a>
