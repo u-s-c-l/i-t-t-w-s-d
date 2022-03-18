67bf0490f5977e6f9e6f87751f415c0213437bc8
ewhite1999 <ewhite1999@gmail.com>
2022-03-18 07:00:36 +0000

    style waiting room

--- src/components/CreateWR/CreateWR.js
+++ src/components/CreateWR/CreateWR.js
@@ -36,18 +36,32 @@ const CreateWR = () => {
       <div>
         {newPlayers.length === 0 ? (
           <>
-            <h2>Waiting for others players to join the game</h2>
+            <h2 className="mx-auto w-72 text-xl text-slate-50 max-w-prose my-12">
+              Waiting for others players to join the game
+            </h2>
           </>
         ) : (
           <div>
-            <h2> Joined:</h2>
+            <h2 className="mx-auto w-72 text-xl text-slate-50 max-w-prose mt-12 mb-4">
+              Joined:
+            </h2>
             {newPlayers.map((newPlayer) => (
-              <h3 key={newPlayer}>{newPlayer}</h3>
+              <h3
+                className={"mx-auto w-72 text-slate-50 max-w-prose"}
+                key={newPlayer}
+              >
+                {newPlayer}
+              </h3>
             ))}
           </div>
         )}
       </div>
-      <a onClick={handleNext}>Play Game</a>
+      <a
+        className="bg-slate-50 text-tblack py-3 text-center rounded-full font-bold w-60 block mx-auto cursor-pointer fixed bottom-24 left-0 right-0"
+        onClick={handleNext}
+      >
+        Play Game
+      </a>
     </div>
   );
 };
