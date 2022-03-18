279f0e1bb906225778399339c59d907816429598
jalexxx <jumaanahalex@gmail.com>
2022-03-18 11:21:24 +0000

    added a top margin

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -6,166 +6,168 @@ const Settings = () => {
   const [privacy, setPrivacy] = useState(true);
   const [general, setGeneral] = useState(true);
   return (
-    <div className="settings-div flex flex-col  p-4 items-center border border-tblack font-extralight  bg-slate-50  justify-around rounded-tl-3xl rounded-tr-3xl max-w-sm m-8  ">
-      <div className="profile_section">
-        <a id="profile_btn" href="#">
-          <i
-            className="far fa-user-circle"
-            onClick={() => setProfile((s) => !s)}
-          >
-            Profile{" "}
+    <div className="pt-8">
+      <div className="settings-div flex flex-col  p-4 items-center border border-tblack font-extralight  bg-slate-50  justify-around rounded-lg max-w-sm m-8 mt-0  ">
+        <div className="profile_section">
+          <a id="profile_btn" href="#">
+            <i
+              className="far fa-user-circle"
+              onClick={() => setProfile((s) => !s)}
+            >
+              Profile{" "}
+            </i>{" "}
+          </a>
+          <div className={profile ? "hidden" : "block"}>
+            <ul>
+              <li>Chat Notifications</li>
+              <div className="m-4">
+                <div className="toggle colour">
+                  <input
+                    id="profile1"
+                    className="toggle-checkbox hidden"
+                    type="checkbox"
+                  ></input>
+                  <label
+                    htmlFor="profile1"
+                    className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                  ></label>
+                </div>
+              </div>
+              <li>Game Notifications</li>
+              <div className="m-4">
+                <div className="toggle colour">
+                  <input
+                    id="profile2"
+                    className="toggle-checkbox hidden"
+                    type="checkbox"
+                  ></input>
+                  <label
+                    htmlFor="profile2"
+                    className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                  ></label>
+                </div>
+              </div>
+              <li>Change Password</li>
+              <div className="m-4">
+                <div className="toggle colour">
+                  <input
+                    id="profile3"
+                    className="toggle-checkbox hidden"
+                    type="checkbox"
+                  ></input>
+                  <label
+                    htmlFor="profile3"
+                    className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                  ></label>
+                </div>
+              </div>
+              <li>Change Username</li>
+              <div className="m-4">
+                <div className="toggle colour">
+                  <input
+                    id="profile4"
+                    className="toggle-checkbox hidden"
+                    type="checkbox"
+                  ></input>
+                  <label
+                    htmlFor="profile4"
+                    className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                  ></label>
+                </div>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <a id="privacy_btn" href="#">
+          <i className="fa fa-lock" onClick={() => setPrivacy((s) => !s)}>
+            Privacy{" "}
           </i>{" "}
         </a>
-        <div className={profile ? "hidden" : "block"}>
+        <div className={privacy ? "hidden" : "block"}>
           <ul>
-            <li>Chat Notifications</li>
+            <li>Accept Chat Requests</li>
             <div className="m-4">
               <div className="toggle colour">
                 <input
-                  id="profile1"
+                  id="privacy1"
                   className="toggle-checkbox hidden"
                   type="checkbox"
                 ></input>
                 <label
-                  htmlFor="profile1"
+                  htmlFor="privacy1"
                   className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
                 ></label>
               </div>
             </div>
-            <li>Game Notifications</li>
+            <li>Anonymous mode </li>
             <div className="m-4">
               <div className="toggle colour">
                 <input
-                  id="profile2"
+                  id="privacy2"
                   className="toggle-checkbox hidden"
                   type="checkbox"
                 ></input>
                 <label
-                  htmlFor="profile2"
+                  htmlFor="privacy2"
                   className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
                 ></label>
               </div>
             </div>
-            <li>Change Password</li>
+            <li>Billing info</li>
             <div className="m-4">
               <div className="toggle colour">
                 <input
-                  id="profile3"
+                  id="privacy3"
                   className="toggle-checkbox hidden"
                   type="checkbox"
                 ></input>
                 <label
-                  htmlFor="profile3"
-                  className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-                ></label>
-              </div>
-            </div>
-            <li>Change Username</li>
-            <div className="m-4">
-              <div className="toggle colour">
-                <input
-                  id="profile4"
-                  className="toggle-checkbox hidden"
-                  type="checkbox"
-                ></input>
-                <label
-                  htmlFor="profile4"
+                  htmlFor="privacy3"
                   className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
                 ></label>
               </div>
             </div>
           </ul>
         </div>
+        <a id="general_btn" href="#">
+          <i className="fas fa-cogs" onClick={() => setGeneral((s) => !s)}>
+            general{" "}
+          </i>{" "}
+        </a>
+        <div className={general ? "hidden" : "block"}>
+          <ul>
+            <li>Help</li>
+            <div className="m-4">
+              <div className="toggle colour">
+                <input
+                  id="general1"
+                  className="toggle-checkbox hidden"
+                  type="checkbox"
+                ></input>
+                <label
+                  htmlFor="general1"
+                  className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                ></label>
+              </div>
+            </div>
+            <li> Compeition Alerts </li>
+            <div className="m-4">
+              <div className="toggle colour">
+                <input
+                  id="general2"
+                  className="toggle-checkbox hidden"
+                  type="checkbox"
+                ></input>
+                <label
+                  htmlFor="general2"
+                  className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
+                ></label>
+              </div>
+            </div>
+          </ul>
+        </div>
+        <button className=" bg-red-700 text-slate-50"> Delete Account </button>
       </div>
-      <a id="privacy_btn" href="#">
-        <i className="fa fa-lock" onClick={() => setPrivacy((s) => !s)}>
-          Privacy{" "}
-        </i>{" "}
-      </a>
-      <div className={privacy ? "hidden" : "block"}>
-        <ul>
-          <li>Accept Chat Requests</li>
-          <div className="m-4">
-            <div className="toggle colour">
-              <input
-                id="privacy1"
-                className="toggle-checkbox hidden"
-                type="checkbox"
-              ></input>
-              <label
-                htmlFor="privacy1"
-                className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-              ></label>
-            </div>
-          </div>
-          <li>Anonymous mode </li>
-          <div className="m-4">
-            <div className="toggle colour">
-              <input
-                id="privacy2"
-                className="toggle-checkbox hidden"
-                type="checkbox"
-              ></input>
-              <label
-                htmlFor="privacy2"
-                className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-              ></label>
-            </div>
-          </div>
-          <li>Billing info</li>
-          <div className="m-4">
-            <div className="toggle colour">
-              <input
-                id="privacy3"
-                className="toggle-checkbox hidden"
-                type="checkbox"
-              ></input>
-              <label
-                htmlFor="privacy3"
-                className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-              ></label>
-            </div>
-          </div>
-        </ul>
-      </div>
-      <a id="general_btn" href="#">
-        <i className="fas fa-cogs" onClick={() => setGeneral((s) => !s)}>
-          general{" "}
-        </i>{" "}
-      </a>
-      <div className={general ? "hidden" : "block"}>
-        <ul>
-          <li>Help</li>
-          <div className="m-4">
-            <div className="toggle colour">
-              <input
-                id="general1"
-                className="toggle-checkbox hidden"
-                type="checkbox"
-              ></input>
-              <label
-                htmlFor="general1"
-                className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-              ></label>
-            </div>
-          </div>
-          <li> Compeition Alerts </li>
-          <div className="m-4">
-            <div className="toggle colour">
-              <input
-                id="general2"
-                className="toggle-checkbox hidden"
-                type="checkbox"
-              ></input>
-              <label
-                htmlFor="general2"
-                className="toggle-label block w-12 h-6 rounded-full transition-color duration-150 ease-out"
-              ></label>
-            </div>
-          </div>
-        </ul>
-      </div>
-      <button className=" bg-red-700 text-slate-50"> Delete Account </button>
     </div>
   );
 };
