3da184f3418a9553a11691b084eddf3850c84a92
jalexxx <jumaanahalex@gmail.com>
2022-03-18 10:46:59 +0000

    aign icons in center of divs

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -2,12 +2,8 @@ import React from "react";
 
 const Chat = () => {
   return (
-    <div>
-      <a
-        id="chat_btn"
-        href="/ChatRoom"
-        className="bg-slate-50 w-28 flex justify-around rounded-3xl max-w-sm"
-      >
+    <div className="bg-slate-50 w-28 h-10  flex justify-around rounded-3xl max-w-sm">
+      <a id="chat_btn" href="/ChatRoom" className="my-auto">
         <i className="fa-solid fa-hand-holding-heart fa-2x"></i>
       </a>
     </div>
--- src/components/SettingsBtn/index.js
+++ src/components/SettingsBtn/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const SettingsBtn = () => {
   return (
     <div className="bg-slate-50 w-28 flex justify-around rounded-3xl max-w-sm">
-      <a id="settings_btn" href="/Settings">
+      <a id="settings_btn" href="/Settings" className="my-auto">
         <i className="fa-solid fa-gear fa-2x "></i>
       </a>
     </div>
