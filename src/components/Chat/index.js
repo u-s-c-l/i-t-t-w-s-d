364df47e9ae14dbf1897eb0fdeffe11e8a3461fe
jalexxx <jumaanahalex@gmail.com>
2022-03-18 10:40:19 +0000

    made icon for chat bigger

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,8 +3,12 @@ import React from "react";
 const Chat = () => {
   return (
     <div>
-      <a id="chat_btn" href="/ChatRoom">
-        <i className="fa-solid fa-hand-holding-heart"></i>
+      <a
+        id="chat_btn"
+        href="/ChatRoom"
+        className="bg-slate-50 w-28 flex justify-around rounded-3xl max-w-sm"
+      >
+        <i className="fa-solid fa-hand-holding-heart fa-2x"></i>
       </a>
     </div>
   );
