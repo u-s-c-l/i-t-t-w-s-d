91ae7365cd628333522ced3ffffc82fb3d49facc
saminakhan999 <samina.khan9999@gmail.com>
2022-03-19 19:59:15 +0000

    changed chat socket

--- src/pages/PlayGame/PlayGame.js
+++ src/pages/PlayGame/PlayGame.js
@@ -8,6 +8,14 @@ const PlayGame = () => {
   const dispatch = useDispatch();
   const socket = useSocket();
 
+
+    useEffect(() => {
+      socket.on("chat-message", (username, msg) => {
+        io.emit("the-message", username + ": " + msg);
+      });
+    }, [socket]);
+
+
   useEffect(() => {
     socket.on("end-game", (username, score) => {
       dispatch(recordPlayerResult(username, score));
